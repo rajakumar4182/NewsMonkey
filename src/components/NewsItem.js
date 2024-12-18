4088c31793b44a7c0f49ce6c1ba1706bd08522c4
Raja Kumar <rajakumar@Rajas-MacBook-Air.local>
2024-12-18 10:22:43 +0530

    This is a class react based latest news website

---
+++ src/components/NewsItem.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+
+export class NewsItem extends Component {
+  render() {
+    let { title, description, imageUrl, newsUrl } = this.props;
+    return (
+      <div className="my-3">
+        <div className="card" style={{ width: "18rem" }}>
+          <img
+            src={
+              !imageUrl
+                ? "https://m-cdn.phonearena.com/images/article/165890-wide-two_1200/Samsungs-Galaxy-S25-Ultra-color-palette-leaks-out-in-unprecedented-detail.jpg"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default NewsItem;
