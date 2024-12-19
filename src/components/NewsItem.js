7f1fc504fda8f864c4d22dbf71ffc186716e2b68
Raja Kumar <rajakumar@Rajas-MacBook-Air.local>
2024-12-19 18:00:24 +0530

    added badges and source and author and date

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,10 +2,17 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl } = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      this.props;
     return (
       <div className="my-3">
         <div className="card">
+          <span
+            class="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
+            style={{ left: "90%", zIndex: "1" }}
+          >
+            {source}
+          </span>
           <img
             src={
               !imageUrl
@@ -18,6 +25,12 @@ export class NewsItem extends Component {
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
+            <p className="card-text">
+              <small className="text-body-secondary">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
             <a
               rel="noreferrer"
               href={newsUrl}
